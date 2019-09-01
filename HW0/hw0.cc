415f19f3cd27a604a82422e666abf1d570ccacbd
Yanjun <45958473+jyj0john@users.noreply.github.com>
2019-09-01 09:14:42 -0600

    Add files via upload

---
+++ HW0/hw0.cc
@@ -0,0 +1,8 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"it take zero Colledge students to change a light bulb, as they will call the faculty stuff to change it"<<endl;
+return 0;
+}
+
